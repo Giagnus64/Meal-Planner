551ebaddbffd66cbea8ad522c57733c10169da08
Giagnus64 <28762593+Giagnus64@users.noreply.github.com>
2018-09-13 01:44:37 -0400

    Added URL validation on recipe URLs

--- src/ui.js
+++ src/ui.js
@@ -11,10 +11,10 @@ class UI{
 					  	<div class="card-body">
 						    <h5 class="card-title">${date}</h5>
 						    <h6 class="card-subtitle text-muted">Breakfast</h6>
-						    <p class="card-text"><a href="www.allrecipes.com" class="recipe-link ">Avocado Toast with Eggs</a></p>
+						    <p class="card-text"><a target="_blank" href="http://www.allrecipes.com" class="recipe-link ">Avocado Toast with Eggs</a></p>
 						    <h6 class="card-subtitle text-muted">Lunch</h6>
-						    <p class="card-text"><a class="recipe-link ">Chicken Cutlet with Broccoli Rabe</a></p><h6 class="card-subtitle text-muted">Dinner</h6>
-						    <p class="card-text"><a class="recipe-link">Recipe Title</a></p>
+						    <p class="card-text"><a class="recipe-link " target="_blank">Chicken Cutlet with Broccoli Rabe</a></p><h6 class="card-subtitle text-muted">Dinner</h6>
+						    <p class="card-text"><a class="recipe-link" target="_blank">Recipe Title</a></p>
 						    	<button class="card-link btn btn-outline-primary card-day-edit">Edit</button>
 					 	 </div>
 					</div>
@@ -54,36 +54,86 @@ class UI{
 		editButton.disabled = false;
 	}
 
-	saveEdits(e){
-		//re-enable other edits buttons and change styles accordingly
-		const otherButtons = document.querySelectorAll('.card-day-edit');
-		otherButtons.forEach(button =>{
-			button.classList.remove("disabled");
-			button.disabled = false;
-		})		
+	saveEdits(e){	
 		//target necessary card elements
 		const card = e.target.parentElement;
 		const mealList = card.querySelectorAll(".card-text");
 		const mealInputs = card.querySelectorAll(".input-meal");
 		const linkInputs = card.querySelectorAll(".input-link");
+
+		//validates URLs and adds error messages
+		let linksValid = this.validateURLs(linkInputs);
+		
+		//cease function if URL not valid
+		if(!linksValid){
+			return false;
+		}
+
 		//iterate over card-elements and place corresponding meal texts and link attributes
 		for(let i = 0; i<3; i++){
-			mealList[i].innerHTML = `<a href="" target=_blank class="recipe-link">${mealInputs[i].value}</a>`
-			if(linkInputs[i].value !== '' && linkInputs[i].value !=='Recipe Link'){
-				mealList[i].setAttribute('href', linkInputs[i].value)
-			}
-
+			mealList[i].innerHTML = `<a target=_blank class="recipe-link">${mealInputs[i].value}</a>`;
+			if(this.is_url(linkInputs[i].value)){
+				mealList[i].children[0].setAttribute('href', linkInputs[i].value);
+			} 
 		}
+		//re-enable other edits buttons and change styles accordingly
+		const otherButtons = document.querySelectorAll('.card-day-edit');
+		otherButtons.forEach(button =>{
+			button.classList.remove("disabled");
+			button.disabled = false;
+		})
 		//change save button back to edit button and add corresponding styles
 		const saveButton = e.target;
 		saveButton.innerText = "Edit";
 		saveButton.classList.remove("card-day-save", "btn-outline-info");
-		saveButton.classList.add("card-day-edit", "btn-outline-primary");
-		
-
+		saveButton.classList.add("card-day-edit", "btn-outline-primary");		
 	}
 
+	//Verfies that link inserted by user is a proper URL
+	//taken from https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
+	is_url(str){
+  		const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str)){
+          return true;
+        }
+        else{
+          return false;
+        }
+	}
+	linkError(link){
+		//creates and adds a new div with a new textNode and inserts it under the link element
+		const div = document.createElement('div');
+		const text = document.createTextNode('Please enter a valid URL or leave this space blank.');
+		div.appendChild(text);
+		div.classList.add("invalid-feedback");
+		//checks for error message and replaces it
+		if(link.parentElement.children.length > 1){
+			link.parentElement.replaceChild(div,link.parentElement.lastChild);
+		} else{
+			link.parentElement.appendChild(div);
+		}
+		return 0;
+	}
+	validateURLs(linkArray){
+		//validatesURL's entered and throws error if not valid	
+		let linksValid = true;
+		linkArray.forEach(link =>{
+			if(link.value !== ''){
+				if((!this.is_url(link.value))){
+					link.classList.add("is-invalid");
+					this.linkError(link);
+					linksValid = false;
+					return false;
+				} else{
+					return true
+				}
+			} 
+		});
+		return linksValid;
+		
+	}
 
 }
+
 
 export const ui = new UI();
